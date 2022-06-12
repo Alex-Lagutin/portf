e8528657cce1fa1921a962f139cc5035efd05c82
Alex-Lagutin <feersan@mail.ru>
2022-06-12 20:31:22 +0300

    Добавил кликабельности меню

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(function(){
-  var mixer = mixitup('.portfolio__content');
-});
 
-$(function(){
-  $('.menu__btn').on('click', function(){
+	$(".menu a, .header__arrow, .logo ").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+		var mixer = mixitup('.portfolio__content');
+	});
+
+
+  $('.menu__btn, .menu a').on('click', function(){
     $('.menu__btn,.menu').toggleClass('active');
     $('body').toggleClass('lock');
-  });
+  
 });
+
