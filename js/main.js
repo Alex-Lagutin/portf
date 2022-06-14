3ae5bca93b9598362ff66bbb19e086d08080d764
Alex-Lagutin <feersan@mail.ru>
2022-06-14 23:23:40 +0300

    поправил хедер

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 $(function(){
 
-	$(".menu a, .header__arrow, .logo ").on("click", function (event) {
+	$(window).scroll(function() {
+		if ($(window).scrollTop() >=200) {
+				$('.header').addClass('sticky', 'header__top--sticky')	
+		} else {
+				$('.header').removeClass('sticky', 'header__top--sticky')
+		}
+			if ($(window).scrollTop() >=200) {
+				$('.header__top').addClass('header__top--sticky')	
+		} else {
+				$('.header__top').removeClass('header__top--sticky')
+		}
+	}); 
+
+	const headerHeight = $('.header').outerHeight();
+
+	$('.menu a, .header__arrow, .logo ').on("click", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
+			top = $(id).offset().top - headerHeight;
 		$('body,html').animate({scrollTop: top}, 1500);
+		
 	});
 
 		var mixer = mixitup('.portfolio__content');
